39f413e758df48872e3c2ee5acb7d1fc4dece2fc
Life90 <68822929+Life90@users.noreply.github.com>
2022-09-12 23:23:38 -0700

    added authentication methods

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import App from "./App";
-
+import { UserProvider } from "./contexts/user.context";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
